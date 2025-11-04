e6eec45640740fac16c15180592ac12580243356
justin <kimsuungjuu@gmail.com>
2025-11-04 15:18:40 -0800

    Initial commit

---
+++ modules/web-server/outputs.tf
@@ -0,0 +1,3 @@
+output "instance_ip_address" { value = aws_instance.web.public_ip }
+output "instance_dns_name"   { value = aws_instance.web.public_dns }
+output "instance_id"         { value = aws_instance.web.id }
---
+++ modules/web-server/variables.tf
@@ -0,0 +1,9 @@
+variable "project_name" { type = string }
+variable "ami_id" { type = string }
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+variable "key_name" { type = string }
+variable "vpc_security_group_ids" { type = list(string) }
+variable "subnet_id" { type = string }
